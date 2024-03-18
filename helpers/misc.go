acd62f44301f4ffee4dc716a554090d6982cc792
Austin Abro <AustinAbro321@gmail.com>
2024-03-18 13:49:04 +0000

    feat(helpers): adding new helper function

--- helpers/misc.go
+++ helpers/misc.go
@@ -76,6 +76,7 @@ func MergeMapRecursive(m1, m2 map[string]interface{}) (r map[string]interface{})
 	}
 	
 
+
 	for key, value := range m2 {
 		if value, ok := value.(map[string]interface{}); ok {
 			if nestedValue, ok := r[key]; ok {
